dd61cd9976ba4d47b57208817a7495d232e53a8b
A-Glazer <avigayilglazer@gmail.com>
2020-02-19 15:18:55 +0200

    imported BabysitterContainer into app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import {fetchBabysitters} from './actions/fetchBabysitters'
-
+import BabysittersContainer from './containers/BabysittersContainer'
 
 class App extends React.Component {
 
@@ -9,7 +8,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-      App
+        <BabysittersContainer />
     </div>
   );
 }
@@ -17,7 +16,7 @@ class App extends React.Component {
 
 
 
-export default connect(null, {fetchBabysitters})(App);
+export default connect()(App);
 // we don't need to get anything from the store, so we don't need 
 // mapStateToProps. However, we want to update the store so we need
 // mapDispatchToProps
