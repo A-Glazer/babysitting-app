87af3bfe78a2f1ee96eaf766815f9fa7791915ca
A-Glazer <avigayilglazer@gmail.com>
2020-02-19 12:49:19 +0200

    imported babysitterReducer into index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
+import babysitterReducer from './reducers/babysitterReducer'
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(babysitterReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
     <Provider store={store}>
