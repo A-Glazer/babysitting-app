3f1042f7f49b563b93ccf4dd23e29735d1512c90
A-Glazer <avigayilglazer@gmail.com>
2020-02-19 15:23:12 +0200

    Set up BabysitterInput.js component

---
+++ src/components/BabysitterInput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default class BabysitterInput extends React.Component {
+
+    render() {
+        return(
+            <div>BabysitterInput</div>
+        )
+    }
+}
