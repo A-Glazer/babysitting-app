91238bb1e049c8546eec9d3d8c5265327024eada
A-Glazer <avigayilglazer@gmail.com>
2020-02-20 21:27:49 +0200

    set up addBabysitter fetch post request

--- src/actions/addBabysitter.js
+++ src/actions/addBabysitter.js
@@ -1,7 +1,15 @@
 export const addBabysitter = data => {
-   debugger
-    
-    return("hi")
+   
+    return(dispatch) => {
+        fetch('http://localhost:3000/api/v1/babysitters', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+    }
 
 
 }
