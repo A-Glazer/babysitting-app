468ff3eed9e31a34f7ab405aceeb22c8303684a6
A-Glazer <avigayilglazer@gmail.com>
2020-02-23 12:36:33 +0200

    Added switch in babysitterContainer, took out exact path

--- src/components/BabysitterShow.js
+++ src/components/BabysitterShow.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Redirect} from 'react-router-dom'
 
 const BabysitterShow = (props) => {
     let babysitter = props.babysitters[props.match.params.id -1]
--- src/containers/BabysittersContainer.js
+++ src/containers/BabysittersContainer.js
@@ -3,7 +3,7 @@ import BabysitterInput from '../components/BabysitterInput'
 import Babysitters from '../components/Babysitters'
 import {fetchBabysitters} from '../actions/fetchBabysitters'
 import { connect } from 'react-redux'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import BabysitterShow from '../components/BabysitterShow'
 
 
@@ -16,12 +16,14 @@ class BabysittersContainer extends React.Component {
     render() {
         return(
             <div>
-                <Route path='/babysitters/new' component={BabysitterInput}/>
-                <Route path='/babysitters/:id' render={(routerProps) => 
-                    <BabysitterShow {...routerProps} babysitters={this.props.babysitters} /> }   />
-                <Route exact path='/babysitters' render={(routerProps) => 
+                <Switch>
+                    <Route path='/babysitters/new' component={BabysitterInput}/>
+                    <Route path='/babysitters/:id' render={(routerProps) => 
+                        <BabysitterShow {...routerProps} babysitters={this.props.babysitters} /> }   />
+                    <Route path='/babysitters' render={(routerProps) => 
                         <Babysitters {...routerProps} babysitters={this.props.babysitters} /> } />
                 
+                </Switch>
             </div>
         )
     }
