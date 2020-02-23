e51c63fdc771e0e08c4111fcc80398c423bb8d80
A-Glazer <avigayilglazer@gmail.com>
2020-02-23 16:11:02 +0200

    Added comments information to comments page

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -4,7 +4,11 @@ const Comments = (props) => {
 
     return (
         <div>
-            Comments
+            {props.comments && props.comments.map(comment =>
+                <li key={comment.id}>{comment.message} - 
+                    <i>{comment.name ? comment.name : null} {comment.created_at}</i></li>
+                
+            )}
         </div>
     )
 
