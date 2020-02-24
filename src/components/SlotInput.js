b848a4ec396298420cb6170a181d7998bbdd26aa
A-Glazer <avigayilglazer@gmail.com>
2020-02-24 21:39:17 +0200

    append slot to babysitter show page list using add_slot

--- src/components/SlotInput.js
+++ src/components/SlotInput.js
@@ -32,21 +32,21 @@ handleOnSubmit = event => {
                     <label>Day Availability: </label>
                     <select name="day_of_week" value={this.state.day_of_week} onChange={this.handleOnChange}>
                         <option>Select Day</option>
-                        <option value="0">Sunday</option>
-                        <option value="1">Monday</option>
-                        <option value="2">Tuesday</option>
-                        <option value="3">Wednesday</option>
-                        <option value="4">Thursday</option>
-                        <option value="5">Friday</option>
-                        <option value="6">Saturday</option>
+                        <option value="0" key={0}>Sunday</option>
+                        <option value="1" key={1}>Monday</option>
+                        <option value="2" key={2}>Tuesday</option>
+                        <option value="3" key={3}>Wednesday</option>
+                        <option value="4" key={4}>Thursday</option>
+                        <option value="5" key={5}>Friday</option>
+                        <option value="6" key={6}>Saturday</option>
                     </select>
                     <label>Time Availability: </label>
                     <select name="time_of_day" value={this.state.time_of_day} onChange={this.handleOnChange}>
                         {/* need to iterate over times */}
                        <option>Select Time</option>
-                       <option value="0">Morning</option> 
-                       <option value="1"> Afternoon</option>
-                       <option value="2">Evening</option>
+                       <option value="0" key={0}>Morning</option> 
+                       <option value="1" key={1}> Afternoon</option>
+                       <option value="2" key={2}>Evening</option>
                     </select>
                     <input type="submit"/>
                 </form>
