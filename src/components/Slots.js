a400e5f9d9965b527b83fbd8733bf0e95094fea4
A-Glazer <avigayilglazer@gmail.com>
2020-02-24 12:29:51 +0200

    Added fxn dayToString and timeToString to display slots as strings

--- src/components/Slots.js
+++ src/components/Slots.js
@@ -2,15 +2,42 @@ import React from 'react'
 
 const Slots = (props) => {
 
+    function dayToString(day_of_week){
+        if (day_of_week === 0){
+            return "Sunday"
+        }else if (day_of_week === 1){
+            return "Monday"
+        }else if (day_of_week === 2){
+            return "Tuesday"
+        }else if (day_of_week === 3){
+            return "Wednesday"
+        }else if (day_of_week === 4){
+            return "Thursday"
+        }else if (day_of_week == 5){
+            return "Friday"
+        }else if (day_of_week === 6){
+            return "Saturday"
+        }
+    }
+
+    function timeToString(time_of_day){
+        if (time_of_day === 0){
+            return "Morning"
+        }else if (time_of_day === 1){
+            return "Afternoon"
+        }else if (time_of_day === 2){
+            return "Evening"
+        }
+
+    }
+
     return (
         <div>
             <button>Add Another Time</button>
             {props.slots && props.slots.map(slot =>
-            // console.log(slot),
             <div>
-                {slot.day_of_week}-{slot.time_of_day}
+                {dayToString(slot.day_of_week)} - {timeToString(slot.time_of_day)}
             </div>    
-            // <li key={slot.id}>{slot} </li>
             
             )}
         </div>
