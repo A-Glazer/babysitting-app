f0d88c910f0b66b853a32ca3f9242fa2ed1a54c6
A-Glazer <avigayilglazer@gmail.com>
2020-02-26 21:56:21 +0200

    Added style to New Babysitter Form

--- src/components/BabysitterInput.js
+++ src/components/BabysitterInput.js
@@ -2,6 +2,11 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {addBabysitter} from '../actions/addBabysitter'
 import Flip from 'react-reveal/Flip';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Fade from 'react-reveal/Fade';
+
 // import SlotInput from './SlotInput'
 
  class BabysitterInput extends React.Component {
@@ -33,24 +38,29 @@ import Flip from 'react-reveal/Flip';
     render() {
         return(
             <div>
-
+                <br/>
             <Flip left>
                 <h1 className="flipTitle">Create a New Babysitter:</h1>
             </Flip>
+            <br/>
+            <div className="formDiv">
                 <form onSubmit={this.handleOnSubmit} >
-                    <label>First Name: </label>
-                    <input type="text" placeholder="First Name" value={this.state.first_name} name="first_name" onChange={this.handleOnChange} />
+                <Fade top cascade >
+                    <label>First Name:  </label>
+                    <input type="text" placeholder= " First Name" value={this.state.first_name} name="first_name" onChange={this.handleOnChange} />
                     <br/><br/>
-                    <label>Last Name: </label>
-                    <input type="text" placeholder="Last Name" value={this.state.last_name} name="last_name" onChange={this.handleOnChange}/>
+                    <label>Last Name:  </label>
+                    <input type="text" placeholder=" Last Name" value={this.state.last_name} name="last_name" onChange={this.handleOnChange}/>
                     <br/><br/>
-                    <label>Phone Number: </label>
-                    <input type="text" placeholder="Phone Number" value={this.state.phone_number} name="phone_number" onChange={this.handleOnChange}/>
+                    <label>Phone Number:  </label>
+                    <input type="text" placeholder=" Phone Number" value={this.state.phone_number} name="phone_number" onChange={this.handleOnChange}/>
                     <br/><br/>
                     {/* <SlotInput babysitter={this.props.babysitter}/> */}
                     <input type="submit"/>
                     
-                </form>
+                </Fade>
+                    </form>
+                </div>
             </div>
         )
     }
