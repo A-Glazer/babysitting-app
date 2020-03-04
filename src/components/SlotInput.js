9782113ff6b1923941c121e189d5b178ac62cd15
A-Glazer <avigayilglazer@gmail.com>
2020-03-04 12:59:19 +0200

    Added styling to slot input

--- src/components/SlotInput.js
+++ src/components/SlotInput.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addSlot } from '../actions/addSlot'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
 
 class SlotInput extends React.Component {
 
@@ -16,10 +18,10 @@ class SlotInput extends React.Component {
 
     }
 
-    validateForm(event){
-        if (event.target.day_of_week.value === 'Select Day' || event.target.day_of_week.value === '' || event.target.time_of_day.value === '' || event.target.time_of_day.value === 'Select Time' ) {
+    validateForm(event) {
+        if (event.target.day_of_week.value === 'Select Day' || event.target.day_of_week.value === '' || event.target.time_of_day.value === '' || event.target.time_of_day.value === 'Select Time') {
             alert("Please select a day and time")
-            
+
         }
     }
 
@@ -37,8 +39,8 @@ class SlotInput extends React.Component {
         return (
             <div>
                 <form onSubmit={this.handleOnSubmit}>
-                    <label>Day Availability: </label>
-                    <select name="day_of_week" value={this.state.day_of_week} onChange={this.handleOnChange} required min="0" max="6">
+                    <label className="addSpace">Day Availability: </label>
+                    <select name="day_of_week" value={this.state.day_of_week} onChange={this.handleOnChange} required min="0" max="6" >
                         <option>Select Day</option>
                         <option value="0" key={0}>Sunday</option>
                         <option value="1" key={1}>Monday</option>
@@ -48,16 +50,18 @@ class SlotInput extends React.Component {
                         <option value="5" key={5}>Friday</option>
                         <option value="6" key={6}>Saturday</option>
                     </select>
-                    <label>Time Availability: </label>
+                    
+                    <label className="addSpace">Time Availability: </label>
                     <select name="time_of_day" value={this.state.time_of_day} onChange={this.handleOnChange} required min="0" max="2">
                         <option>Select Time</option>
                         <option value="0" key={0}>Morning</option>
                         <option value="1" key={1}> Afternoon</option>
                         <option value="2" key={2}>Evening</option>
                     </select>
-                    <input type="submit" />
+                    <label className="addSpace">    </label>
+                    <input type="submit"/>
                 </form>
-            </div>
+            </div >
         )
     }
 
