18ec8cd15e16d4396980c9cf5d60cde67b374235
A-Glazer <avigayilglazer@gmail.com>
2020-04-19 13:51:03 -0400

    Auto stash before rebase of "origin/master"

--- src/components/BabysitterCard.js
+++ src/components/BabysitterCard.js
@@ -3,23 +3,33 @@ import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import CardDeck from 'react-bootstrap/CardDeck'
 import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class BabysitterCard extends React.Component {
 
     state = {
-        number: 0
+        upNumber: 0,
+        downNumber: 0
     }
-    
+
     addVote = () => {
-         this.setState(({number}) =>{
-            return {number: number + 1}
-         })
-     }
+        this.setState(({ upNumber }) => {
+            return { upNumber: upNumber + 1 }
+        })
+    }
+
+    addDownVote = () => {
+        this.setState(({ downNumber }) => {
+            return { downNumber: downNumber + 1 }
+        })
+    }
 
     render() {
 
         let { id, first_name, last_name, phone_number } = this.props.babysitter
-        
+
 
         return (
             <div>
@@ -28,13 +38,22 @@ class BabysitterCard extends React.Component {
                         <Card.Body>
                             <Card.Title>{last_name}, {first_name}</Card.Title>
                             <Card.Text>{phone_number}</Card.Text>
-                            <Link to={{pathname: `/babysitters/${id}`}}>
-                                <Button>View Availability</Button><br/>
-                                
+                            <Link to={{ pathname: `/babysitters/${id}` }}>
+                                <Button>View Availability</Button><br />
+
                             </Link>
-                                <Button onClick={this.addVote}>Upvote</Button>
-                                <p>{this.state.number}</p>
                         </Card.Body>
+                        <Card.Footer>
+                            <Container>
+                                <Row>
+                                    <Col>Rating:</Col>
+                                    <Col><p onClick={this.addVote}>Like:{this.state.upNumber}</p></Col>
+                                    <Col><p onClick={this.addDownVote}>Dislike:{this.state.downNumber}</p></Col>
+                                </Row>
+                            </Container>
+                            
+                            {/* Rating: <button onClick={this.addVote}>Like:{this.state.number}</button> */}
+                        </Card.Footer>
                     </Card>
                 </CardDeck>
             </div>
